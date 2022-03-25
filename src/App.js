d801047cfcf6055e4721fff4e6fb6e0d62ee8706
micheaol <micheaol80@gmail.com>
2022-03-25 11:03:56 +0100

    Add route to the APP

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
 import Greeting from './components/Greeting';
 
 import store from './redux/store/store';
 
+
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <Greeting />
+     <Router>
+          <Routes>
+            <Route exact path="/" element={<Greeting />} />
+          </Routes>
+        </Router>
     </div>
     </Provider>
   );
